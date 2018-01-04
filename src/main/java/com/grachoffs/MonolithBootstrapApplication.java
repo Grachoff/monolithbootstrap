14becc1b5e51646a26cbb7d410006568c6d16691
GrachoffSergej <KYJIBEPCTYKAC@mail.ru>
2018-01-04 23:04:29 +0300

    Initial commit

---
+++ src/main/java/com/grachoffs/MonolithBootstrapApplication.java
@@ -0,0 +1,12 @@
+package com.grachoffs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolithBootstrapApplication.class, args);
+	}
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h1>HiWorld!</h1>
+</body>
+</html>
