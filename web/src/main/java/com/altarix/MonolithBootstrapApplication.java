2d4000a516b8485c6320fa31b575e8549ea4ee98
GrachoffSergej <KYJIBEPCTYKAC@mail.ru>
2018-02-06 09:55:33 +0300

    Backbone was improved

--- web/src/main/java/com/altarix/MonolithBootstrapApplication.java
+++ web/src/main/java/com/altarix/MonolithBootstrapApplication.java
@@ -1,11 +1,14 @@
 package com.altarix;
 
+import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,7 +20,12 @@ import java.util.concurrent.ForkJoinPool;
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@Slf4j
 public class MonolithBootstrapApplication {
+
+	@Value("${spring.datasource.driverClassName}")
+	private String nameOfDbDriver;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonolithBootstrapApplication.class, args);
